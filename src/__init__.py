4b183ae552c8ace4ff2650a65ae309040db82e77
christian <38983174+crycriM@users.noreply.github.com>
2026-01-14 09:01:13 +0100

    packaging

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""bot-monitor package."""
+
+__all__ = ["__version__", "main"]
+__version__ = "0.1.0"
+
+def main() -> None:
+    """Entry point for the package (python -m <your_package>)."""
+    print("Hello from <your_package>!")
